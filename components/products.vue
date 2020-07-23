0a93da064e06ef06127d340c68661e183049c9a6
sucheng <943704372@qq.com>
2020-07-24 04:42:35 +0800

    ”完成商品列表组件封装和引用“

--- components/products.vue
+++ components/products.vue
@@ -1,8 +1,84 @@
 <template>
+	<view class="">
+		<view class="products_list">
+			<view class="products_list_on" v-for="(item,index) in ProductData" :key="index" @click="btn(item.id)">
+				<view class="img">
+					<image :src="item.img_url" mode=""></image>
+				</view>
+				<view class="price">
+					<text>￥{{item.sell_price}}</text>
+					<text> ￥{{item.market_price}}</text>
+				</view>
+				<view class="brand">
+					{{item.title}}
+				</view>
+			</view>
+					
+		</view>
+	</view>
 </template>
 
 <script>
+	export default{
+		props:['ProductData'],
+		methods:{
+			btn(id){
+				this.$emit('productsItemClick',id)
+			}
+		},
+		data(){
+			return{
+				
+			}
+		}
+		
+		
+	}
 </script>
 
-<style>
+<style lang="scss">
+	.products_list{
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: space-between;
+		padding:0 15rpx;
+		.products_list_on{
+			padding: 0 10rpx;
+			box-sizing: border-box;
+			background-color: #fff;
+			width: 49%;
+			margin:5rpx 0;
+			.img{
+				width: 80%;
+				height: 300rpx;
+				margin: 0 auto;
+				image{
+					width: 100%;
+					height: 100%;
+				}
+			}
+			.price{
+				color: $global-color;
+				font-size: 40rpx;
+				margin:20rpx 0 5rpx 0;
+				text:nth-child(2){
+					color: #ccc;
+					font-size: 28rpx;
+					margin-left:17rpx;
+					text-decoration: line-through; //花掉属性
+				}
+			},
+			.brand{
+				font-size:28rpx;
+				line-height: 50rpx;
+				// 设置超出两行的文本显示  .....
+				overflow: hidden;
+				text-overflow: ellipsis;
+				display: -webkit-box;
+				-webkit-line-clamp: 2;
+				-webkit-box-orient: vertical;
+			}
+			
+		}
+	}
 </style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -27,21 +27,7 @@
 				<view class="tit">
 					推荐商品
 				</view>
-				<view class="products_list">
-					<view class="products_list_on" v-for="(item,index) in productsList" :key="index">
-						<view class="img">
-							<image :src="item.img_url" mode=""></image>
-						</view>
-						<view class="price">
-							<text>￥{{item.sell_price}}</text>
-							<text> ￥{{item.market_price}}</text>
-						</view>
-						<view class="brand">
-							{{item.title}}
-						</view>
-					</view>
-			
-				</view>
+				<sc-products :ProductData="productsList"></sc-products>
 			</view>
 		</view>
 	</view>
@@ -49,7 +35,11 @@
 
 <script>
 	import {getLunBoApi,getProductsApi} from '@/api/api.js'
+	import products from '@/components/products.vue'
 	export default {
+		components:{
+			scProducts:products
+		},
 		data() {
 			return {
 				title: 'Hello',
@@ -160,50 +150,7 @@
 				font-size: 40rpx;
 			}
 			//商品
-			.products_list{
-				display: flex;
-				flex-wrap: wrap;
-				justify-content: space-between;
-				padding:0 15rpx;
-				.products_list_on{
-					padding: 0 10rpx;
-					box-sizing: border-box;
-					background-color: #fff;
-					width: 49%;
-					margin:5rpx 0;
-					.img{
-						width: 80%;
-						height: 300rpx;
-						margin: 0 auto;
-						image{
-							width: 100%;
-							height: 100%;
-						}
-					}
-					.price{
-						color: $global-color;
-						font-size: 40rpx;
-						margin:20rpx 0 5rpx 0;
-						text:nth-child(2){
-							color: #ccc;
-							font-size: 28rpx;
-							margin-left:17rpx;
-							text-decoration: line-through; //花掉属性
-						}
-					},
-					.brand{
-						font-size:28rpx;
-						line-height: 50rpx;
-						// 设置超出两行的文本显示  .....
-						overflow: hidden;
-						text-overflow: ellipsis;
-						display: -webkit-box;
-						-webkit-line-clamp: 2;
-						-webkit-box-orient: vertical;
-					}
-					
-				}
-			}
+		
 		}
 	
 	}
